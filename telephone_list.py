c45be84e16dc073917b855f3c54ce5831e7c8fc3
Danil <shwedx@Yandex.ru>
2023-08-24 19:54:08 +0300

    Fixed first bug

--- telephone_list.py
+++ telephone_list.py
@@ -20,11 +20,57 @@ while True:
         if(group == 5):
             clear_console()
         if(group == 1):
+            counts = 0
+            with open("groups/family.txt","r",encoding="utf=8") as file:
+                for line in file:
+                    counts += 1
             with open("groups/family.txt","a",encoding="utf=8") as file:
                 name = input("Введите имя:")
                 number = input("Введите номер:")
-                file.write("\n" + name)
+                stringvalue = str(counts)
+                file.write(stringvalue + ")")
+                file.write( name)
                 file.write(r" - " + number)
+                file.write("\n")
+        if(group == 2):
+            with open("groups/friends.txt","r",encoding="utf=8") as file:
+                for line in file:
+                    counts += 1
+            with open("groups/friends.txt","a",encoding="utf=8") as file:
+                name = input("Введите имя:")
+                number = input("Введите номер:")
+                stringvalue = str(counts)
+                file.write(stringvalue + ")")
+                file.write( name)
+                file.write(r" - " + number)
+                file.write("\n")
+        if(group == 3):
+            with open("groups/work.txt","r",encoding="utf=8") as file:
+                for line in file:
+                    counts += 1
+            with open("groups/work.txt","a",encoding="utf=8") as file:
+                name = input("Введите имя:")
+                number = input("Введите номер:")
+                stringvalue = str(counts)
+                file.write(stringvalue + ")")
+                file.write( name)
+                file.write(r" - " + number)
+                file.write("\n")
+        if(group == 4):
+            with open("groups/other.txt","r",encoding="utf=8") as file:
+                for line in file:
+                    counts += 1
+            with open("groups/other.txt","a",encoding="utf=8") as file:
+                name = input("Введите имя:")
+                number = input("Введите номер:")
+                stringvalue = str(counts)
+                file.write(stringvalue + ")")
+                file.write( name)
+                file.write(r" - " + number)
+                file.write("\n")
+    
+
+        
 
         
 
