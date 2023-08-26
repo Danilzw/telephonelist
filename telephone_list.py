6102bf68b3d375b055aaaee56e6b8ced76784d08
Danil <shwedx@Yandex.ru>
2023-08-26 19:19:38 +0300

    done 1.0

--- telephone_list.py
+++ telephone_list.py
@@ -24,53 +24,60 @@ while True:
             clear_console()
         if(group == 1):
             counts = 0
-            with open("groups/family.txt","r",encoding="utf=8") as file:
-                for line in file:
+            with open("groups/family.txt","r",encoding="utf=8") as file1:
+                for line in file1:
                     counts += 1
-            with open("groups/family.txt","a",encoding="utf=8") as file:
+            with open("groups/family.txt","a",encoding="utf=8") as file1:
                 name = input("Введите имя:")
                 number = input("Введите номер:")
                 stringvalue = str(counts)
-                file.write(stringvalue + ")")
-                file.write( name)
-                file.write(r" - " + number + "\n")
+                file1.write(stringvalue + ")")
+                file1.write( name)
+                file1.write(r" - " + number + "\n")
+           
                 
         if(group == 2):
-            with open("groups/friends.txt","r",encoding="utf=8") as file:
-                for line in file:
+            counts = 0
+            with open("groups/friends.txt","r",encoding="utf=8") as file2:
+                for line in file2:
                     counts += 1
-            with open("groups/friends.txt","a",encoding="utf=8") as file:
+            with open("groups/friends.txt","a",encoding="utf=8") as file2:
                 name = input("Введите имя:")
                 number = input("Введите номер:")
                 stringvalue = str(counts)
-                file.write(stringvalue + ")")
-                file.write( name)
-                file.write(r" - " + number)
-                file.write("\n")
+                file2.write(stringvalue + ")")
+                file2.write( name)
+                file2.write(r" - " + number + "\n")
+               
+            
         if(group == 3):
-            with open("groups/work.txt","r",encoding="utf=8") as file:
-                for line in file:
+            counts = 0
+            with open("groups/work.txt","r",encoding="utf=8") as file3:
+                for line in file3:
                     counts += 1
-            with open("groups/work.txt","a",encoding="utf=8") as file:
+            with open("groups/work.txt","a",encoding="utf=8") as file3:
                 name = input("Введите имя:")
                 number = input("Введите номер:")
                 stringvalue = str(counts)
-                file.write(stringvalue + ")")
-                file.write( name)
-                file.write(r" - " + number)
-                file.write("\n")
+                file3.write(stringvalue + ")")
+                file3.write( name)
+                file3.write(r" - " + number + "\n")
+                
+            
         if(group == 4):
-            with open("groups/other.txt","r",encoding="utf=8") as file:
-                for line in file:
+            counts = 0
+            with open("groups/other.txt","r",encoding="utf=8") as file4:
+                for line in file4:
                     counts += 1
-            with open("groups/other.txt","a",encoding="utf=8") as file:
+            with open("groups/other.txt","a",encoding="utf=8") as file4:
                 name = input("Введите имя:")
                 number = input("Введите номер:")
                 stringvalue = str(counts)
-                file.write(stringvalue + ")")
-                file.write( name)
-                file.write(r" - " + number)
-                file.write("\n")
+                file4.write(stringvalue + ")")
+                file4.write( name)
+                file4.write(r" - " + number + "\n")
+                
+            
     # Delete Function
     if(choice == 2):
         clear_console()
@@ -125,6 +132,7 @@ while True:
             with open("groups/other.txt","w",encoding="utf-8") as file:
                 for i in arraytemp:
                     file.write(i)
+        
     #Read Function
     if(choice == 3):
         clear_console()
@@ -143,6 +151,26 @@ while True:
             with open("groups/other.txt","r",encoding="utf-8") as file:
                 print(file.read())
         
+    #Change function
+    if(choice == 4):
+        clear_console()
+        print("\nСемья","|","Друзья","|","Работа","|","Другое","|","Назад","|")
+        group = int(input("Выберете группу(1,5):"))
+        if(group == 1):
+            with open("groups/family.txt","r",encoding="utf-8") as file:
+                print(file.read())
+            arraytemp = []
+            id = int(input("Кого хотите изменить?(id):"))
+            with open("groups/family.txt","r",encoding="utf-8") as file2:
+                for line in file2:
+                    arraytemp.append(line)
+            newname = input("Введите имя:")
+            newnumber = input("Введите номер:")
+            arraytemp[id] = ((newname + " - " + newnumber))
+            with open("groups/family.txt","w",encoding="utf-8") as file:
+                  for i in arraytemp:
+                      file.write(i)
+            
 
 
 
